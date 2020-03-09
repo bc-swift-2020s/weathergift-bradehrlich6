dc714e1c74dda946d89b2928bb11eae1b000ac5a
bradehrlich6 <60119950+bradehrlich6@users.noreply.github.com>
2020-03-09 15:14:30 -0400

    Add files via upload
    Weather Gift commit #1

---
+++ WeatherGift/WeatherGift/LocationListViewController.swift
@@ -0,0 +1,73 @@
+//
+//  LocationListViewController.swift
+//  WeatherGift
+//
+//  Created by Brad Ehrlich on 3/9/20.
+//  Copyright © 2020 Brad Ehrlich. All rights reserved.
+//
+
+import UIKit
+
+class LocationListViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var editBarButton: UIBarButtonItem!
+    @IBOutlet weak var addBarButton: UIBarButtonItem!
+    
+    var weatherLocations: [WeatherLocation] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        var weatherLocation = WeatherLocation(name: "Philadelphia, PA", latitude: 100.0, longitude: 100.0)
+        weatherLocations.append(weatherLocation)
+        weatherLocation = WeatherLocation(name: "Boston, MA", latitude: 100.0, longitude: 100.0)
+        weatherLocations.append(weatherLocation)
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+    }
+
+
+    @IBAction func addBarButtonPressed(_ sender: UIBarButtonItem) {
+    }
+
+    @IBAction func editBarButtonPressed(_ sender: UIBarButtonItem) {
+        if tableView.isEditing {
+            tableView.setEditing(false, animated: true)
+            sender.title = "Edit"
+            addBarButton.isEnabled = true
+        } else {
+            tableView.setEditing(true, animated: true)
+            sender.title = "Done"
+            addBarButton.isEnabled = false
+        }
+    }
+}
+
+extension LocationListViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return weatherLocations.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = weatherLocations[indexPath.row].name 
+        return cell
+    }
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+            weatherLocations.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let itemToMove =  weatherLocations[sourceIndexPath.row]
+         weatherLocations.remove(at: sourceIndexPath.row)
+         weatherLocations.insert(itemToMove, at: destinationIndexPath.row)
+    }
+    
+}
+
---
+++ WeatherGift/WeatherGift/WeatherLocation.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by Brad Ehrlich on 3/9/20.
+//  Copyright © 2020 Brad Ehrlich. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name: String
+    var latitude: Double
+    var longitude: Double 
+}
